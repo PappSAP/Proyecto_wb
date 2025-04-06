b6da7e3e4f52541115bb34af4416741d967f80a9
PappSAP <pponce@quintec.cl>
2025-04-06 21:37:29 +0000

    back20250406_a

--- webapp/controller/View00.controller.js
+++ webapp/controller/View00.controller.js
@@ -1,10 +1,19 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], 
+(Controller, MessageToast) => {
     "use strict";
 
     return Controller.extend("proyectowb.controller.View00", {
         onInit() {
-        }
+        },
+
+        OnPress : function (oEvent) {
+            let oinput01 = oEvent.getParameter("input0");
+            let oinput0 = oEvent.getParameter()
+            //MessageToast.show(oItem.Name);
+            MessageToast.show( 'ingreso ' + oinput0.name );
+    }
     });
 });
