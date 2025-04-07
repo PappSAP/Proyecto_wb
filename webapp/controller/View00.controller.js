a0dcb510d42efdb2acf5afb08cdf4ccd67c0fb03
PappSAP <pponce@quintec.cl>
2025-04-07 20:15:44 +0000

    back20250407_a

--- webapp/controller/View00.controller.js
+++ webapp/controller/View00.controller.js
@@ -1,20 +1,29 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Messaging",
     "sap/m/MessageToast"
 ], 
-(Controller, MessageToast) => {
+(Controller, Messaging, MessageToast) => {
     "use strict";
 
     return Controller.extend("proyectowb.controller.View00", {
         onInit() {
+            let oView = this.getView(),
+                oMM = Messaging;
+
+            oView = setModel( new JSModel({Input0:"", Input1:""}));
+            		// attach handlers for validation errors
+			oMM.registerObject(oView.byId("ValInput0"), true);
+			oMM.registerObject(oView.byId("ValInput1"), true);
+
         },
 
-        OnPress : function (oEvent) {
+/*         OnPress : function (oEvent) {
             let oInput0 = this.getView().byId("Input0");
             let oInput1 = this.getView().byId("Input1");
             //let oinput0 = oEvent.getParameter()
             //MessageToast.show(oItem.Name);
-            MessageToast.show( 'ingreso ' + oInput0.name );
-    }
+            MessageToast.show( 'ingreso ' + oInput0.name ); 
+    } */
     });
 });
